90462ed05b61987f87809413521e92d003d0328d
= <tboggs300@gmail.com>
2023-03-23 11:10:22 -0400

    Add redux-thunk

--- src/store.jsx
+++ src/store.jsx
@@ -2,11 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { reducers } from 'controllers';
 import { apiMiddleware } from 'middleware';
 import { rootReducer } from 'modules';
+import ReduxThunk from 'redux-thunk';
 
 // NOTE: Do not add new module reducers to allReducers -- add them to controllers/reducersController.jsx
 const allReducers = { ...reducers, root: rootReducer };
 const mainReducer = combineReducers(allReducers);
-const middleware = [ apiMiddleware ];
+const middleware = [ apiMiddleware, ReduxThunk ];
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const devTools = process.env.NODE_ENV !== 'production' && reduxDevTools;
